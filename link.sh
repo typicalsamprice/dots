279770888ebfff9aafa664d0756d0df2b3635a51
Sam Price <sam@typicalsamprice.com>
2023-07-11 01:38:32 -0500

    Add gitconfig, rewrite link.sh

--- link.sh
+++ link.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/sh
 
-set -xe
+set -e
 CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
 THIS_DIR="$(dirname $(readlink -e $0))"
 
+link_to_basedir() {
+    if [ -f "${1}/${2}" ]; then
+        return 0
+    fi
+    if [ -d "${1}/${2}" ]; then
+        return 0
+    fi
+    printf "Linking %s to %s ...\n" "${THIS_DIR}/${2}" "${1}/${2}"
+    ln -sf "${THIS_DIR}/${2}" "${1}/${2}"
+}
 link_to_conf_directly() {
-    ln -sf "${THIS_DIR}/${1}" "${CONFIG_DIR}/${1}"
+    link_to_basedir "${CONFIG_DIR}" "${1}"
 }
 
 
 link_to_conf_directly "starship.toml"
 link_to_conf_directly "nvim"
 link_to_conf_directly "fish/config.fish"
+
+link_to_basedir "$HOME" ".gitconfig"
